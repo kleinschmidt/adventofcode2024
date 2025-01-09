1c7bcb5e3d6d470e162bb1ee8ef426bf6b213224
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-01-08 19:57:47 -0500

    part 1

--- day5/main.go
+++ day5/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"slices"
+	// "slices"
 	"strconv"
 	"strings"
 )
@@ -14,12 +14,15 @@ import (
 func main() {
 	bytes, _ := io.ReadAll(os.Stdin)
 	parseInput(bytes)
-	order, _ := parseInput(bytes)
+	order, revOrder, manuals := parseInput(bytes)
 	fmt.Println(order)
+	fmt.Println(revOrder)
+	part1(order, revOrder, manuals)
 }
 
-func parseInput(input []byte) (order map[int][]int, manuals [][]int) {
+func parseInput(input []byte) (order map[int][]int, revOrder map[int][]int, manuals [][]int) {
 	order = make(map[int][]int)
+	revOrder = make(map[int][]int)
 	manuals = make([][]int, 0)
 
 	scanner := bufio.NewScanner(bytes.NewReader(input))
@@ -33,7 +36,7 @@ func parseInput(input []byte) (order map[int][]int, manuals [][]int) {
 		first := _int(fields[0])
 		second := _int(fields[1])
 		order[first] = append(order[first], second)
-		fmt.Println(line, " -> ", fields)
+		revOrder[second] = append(revOrder[second], first)
 	}
 
 	// parse the X,Y,Z,... manuals
@@ -45,7 +48,6 @@ func parseInput(input []byte) (order map[int][]int, manuals [][]int) {
 			manual[i] = _int(f)
 		}
 		manuals = append(manuals, manual)
-		fmt.Println(line, " -> ", manual)		
 	}
 
 	return
@@ -56,18 +58,54 @@ func _int(x string) int {
 	return xx
 }
 
-func part1(order map[int][]int, manuals [][]int) {
+func part1(order map[int][]int, revOrder map[int][]int, manuals [][]int) (total int) {
+	total = 0
 	for _, manual := range manuals {
+		// strategy here is:
+		// three different lookup indices:
+		// - for page number, all pages that must be later
+		// - for page number, all pages that must be earlier
+		// - for page in a manual, the order (index).
+		//
+		// then we iterate through the manual, and check that the index is
+		// less/greater where required
 		index := make(map[int]int)
 		for i, page := range manual {
 			index[page] = i
 		}
 
+		okay := true
+
+		fmt.Println("Checking manual ", manual)
 		for i, page := range manual {
-			orders := order[page]
-			
+			nexts := order[page]
+			prevs := revOrder[page]
+			for _, next := range nexts {
+				if !okay {break}
+				idx, ok := index[next]
+				if ok && idx < i {
+					fmt.Printf("Violation: %d (%d) precedes %d (%d)\n", next, idx, page, i)
+					okay = false
+				}
+			}
+
+			if !okay {break}
+			for _, prev := range prevs {
+				if !okay {break}
+				idx, ok := index[prev]
+				if ok && idx > i {
+					fmt.Printf("Violation: %d (%d) follows %d (%d)\n", prev, idx, page, i)
+					okay = false
+				}
+			}					
+		}
+
+		if okay {
+			total += manual[len(manual) / 2]
 		}
 	}
+	fmt.Println("Total of middle pages: ", total)
+	return total
 }
 
 func part2() {}
