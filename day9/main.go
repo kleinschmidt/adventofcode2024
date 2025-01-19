9d5a1f97846566fdd64349d2276fcc48c34502f3
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-01-19 15:48:02 -0500

    part 2

--- day9/main.go
+++ day9/main.go
@@ -63,7 +63,8 @@ func parseInput() ([]int, int) {
 
 func main() {
 	input, total := parseInput()
-	part1(input, total)
+	// part1(input, total)
+	part2(input, total)
 }
 
 func feed(c chan int, input []int, revPos int) {
@@ -108,4 +109,94 @@ func part1(input []int, revPos int) int {
 	}
 	fmt.Println("Total:", total)
 	return total
+}
+
+// left closed/right open interval
+type Interval struct {
+	start, stop int
+}
+
+func size(i Interval) int {
+	return i.stop - i.start
+}
+
+func checksum(i Interval, fileNum int) int {
+	return (i.stop + i.start - 1) * size(i) * fileNum / 2
+}
+
+func update(board []byte, i Interval, char byte) {
+	for j := i.start; j < i.stop; j++ {
+		board[j] = char
+	}
+}
+
+func part2(input []int, total int) {
+	// process files in reverse order
+	// move to first contiguous open block that's at least as large
+
+	// need to _iterate_ files, _update_ their positions
+	// need to _shrink_ or _remove_ gaps that are filled, and _join_ gaps that are vacated
+
+	files := make([]Interval, 0)
+	gaps := make([]Interval, 0)
+	pos := 0
+	// board := make([]byte, total)
+	// var char byte
+	for i, size := range input {
+		interval := Interval{pos, pos + size}
+		pos += size
+		switch i % 2 {
+		case 0:
+			files = append(files, interval)
+			// char = byte(i/2) + '0'
+		case 1:
+			gaps = append(gaps, interval)
+			// char = '.'
+		}
+		// update(board, interval, char)
+	}
+
+	// fmt.Println(string(board))
+
+	check := 0
+
+	for fileNum := len(files) - 1; fileNum >= 0; fileNum-- {
+		file := files[fileNum]
+		fileSize := size(file)
+		// fmt.Printf("File %v at %v (size %v)\n", fileNum, file, fileSize)
+		for gapIdx, gap := range gaps {
+			// fmt.Printf("  Gap %v at %v (size %v)\n", gapIdx, gap, size(gap))
+			if gap.start >= file.start {
+				break
+			} else if size(gap) >= fileSize {
+				// update(board, file, '.')
+				file = Interval{gap.start, gap.start + fileSize}
+				// update(board, file, byte(fileNum)+'0')
+				gap = Interval{gap.start + fileSize, gap.stop}
+				// fmt.Printf("  Moving file to %v, new gap: %v\n", file, gap)
+				gaps[gapIdx] = gap
+				// fmt.Println(string(board))
+				break
+			}
+		}
+		cs := checksum(file, fileNum)
+		// fmt.Printf("Checksum for file %v at %v: %v\n", fileNum, file, cs)
+		check += cs
+	}
+
+	fmt.Println("Total checksum:", check)
+
+	// files can only be 1-9 in size.  gaps may become bigger than that!  we
+	// only ever need to know the _first_ gap of a given size, so we can keep an
+	// index of "first gap that can accomodate a file of size X" into the list
+	// of gaps (e.g.,
+	//
+	// then for each file of size F:
+	//   get the first gap that is big enough, of size G starting at g
+	//   advance start of gap g+=F
+	//   update the gap index:
+	//     iterate gaps g' starting at g
+	//     if idx[size(g')] > g', set it to g'
+	//     if size(g') >= G, stop
+
 }
