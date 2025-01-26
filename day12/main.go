a522d945da977a475c7f61ef38262b47377f9533
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2025-01-26 16:15:17 -0500

    furiously debugging

--- day12/main.go
+++ day12/main.go
@@ -26,7 +26,7 @@ func parseInput() ([][]byte, Point) {
 func main() {
 	board, size := parseInput()
 	for _, line := range board {
-		fmt.Println(line)
+		fmt.Println(string(line))
 	}
 	fmt.Println(size)
 
@@ -70,6 +70,7 @@ func get(board [][]byte, p Point) byte {
 	if inbounds(p, board) {
 		return board[p.y][p.x]
 	} else {
+		// use 0 as a sentinel value
 		return byte(0)
 	}
 }
@@ -78,23 +79,32 @@ func visit(start Point, board [][]byte, visited [][]bool) (area, perimeter int) 
 	area = 0
 	perimeter = 0
 
+	// stack := []Point{start}
 	queue := new(queues.Queue[Point])
 	queue.Enqueue(start)
 
 	for queue.HasNext() {
 		cur := queue.Dequeue()
-		// fmt.Println(cur)
+		// for len(stack) > 0 {
+		// 	// pointer just to match the queue
+		// 	cur := &stack[len(stack)-1]
+		// 	stack = stack[:len(stack)-1]
+		// sicne this location was enqueued, it may have been visited from
+		// another neighbor.
 		if visited[cur.y][cur.x] {
-			// fmt.Println("visited")
 			continue
 		}
 		for next := range neighbors(*cur) {
 			// fmt.Print("  ", next)
 			if !inbounds(next, board) || board[next.y][next.x] != board[cur.y][cur.x] {
-				// every out-of-bounds point can only be visited from one in-bounds point
+				// every out-of-bounds point can only be visited from one
+				// in-bounds point ...wait that's not true, if you have a
+				// u-shaped region.  but still, every edge that has a different
+				// species is a length of fence you need...
 				perimeter++
 			} else if !visited[next.y][next.x] {
 				queue.Enqueue(next)
+				// stack = append(stack, next)
 			}
 		}
 		visited[cur.y][cur.x] = true
@@ -104,6 +114,34 @@ func visit(start Point, board [][]byte, visited [][]bool) (area, perimeter int) 
 	return area, perimeter
 }
 
+func printBoard(board [][]byte, visited [][]bool) {
+	for y := range board {
+		for x := range board[y] {
+			if visited[y][x] {
+				fmt.Print(".")
+			} else {
+				fmt.Print(string(board[y][x]))
+			}
+		}
+		println()
+	}
+}
+
+func totalPerimeter(board [][]byte) int {
+	total := 0
+	for y := range board {
+		for x := range board[y] {
+			cur := Point{x, y}
+			for neighbor := range neighbors(cur) {
+				if get(board, neighbor) != get(board, cur) {
+					total++
+				}
+			}
+		}
+	}
+	return total
+}
+
 func part1(board [][]byte, size Point) {
 	visited := make([][]bool, size.y)
 	for i := range visited {
@@ -111,17 +149,24 @@ func part1(board [][]byte, size Point) {
 	}
 
 	total := 0
+	total_perimeter := 0
+	total_area := 0
 
 	for {
 		next := nextUnvisited(visited)
 		if next == size {
 			break
 		}
-		fmt.Println("Visiting", next)
 		area, perimeter := visit(next, board, visited)
-		fmt.Println("Area", area, "perimeter", perimeter)
+		total_area += area
+		total_perimeter += perimeter
+		fmt.Printf("Plot %v %v: area=%d, perimeter=%d\n", get(board, next), next, area, perimeter)
 		total += area * perimeter
 	}
 
+	// printBoard(board, visited)
+
 	fmt.Println("total:", total)
+	fmt.Println("total_area:", total_area, "expected", size.x*size.y)
+	fmt.Println("total_perimeter:", total_perimeter, "expected", totalPerimeter(board))
 }
